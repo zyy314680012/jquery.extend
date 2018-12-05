9784c6c7d86679c8cd83f7992eb7a98e8ac819ff
zhangyongyong <zyy123456>
2018-12-05 12:05:30 +0800

    first commit

---
+++ jquery.extend.js
@@ -0,0 +1,25 @@
+$.extend({
+    loopAjax: function (array, ajaxCfg, finishFunc) {
+        function _loopAjax(ix, array, ajaxCfg, finishFunc) {
+            var total = array.length;
+            if (ix >= total) {
+                finishFunc();
+                return;
+            }
+            var data = $.extend({}, ajaxCfg.data || {}, array[ix]);
+            var func = ajaxCfg.success || function (data) {
+                };
+            var successFunc = function (data) {
+                func(data);
+                _loopAjax(ix + 1, array, ajaxCfg, finishFunc);
+            };
+            var newAjaxParams = $.extend({}, {dataType: 'json', type: 'get'}, ajaxCfg, {
+                data: data,
+                success: successFunc
+            });
+            $.ajax(newAjaxParams);
+        }
+
+        _loopAjax(0, array, ajaxCfg, finishFunc);
+    }
+})

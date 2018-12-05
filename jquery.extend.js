78ef077ae623e4841a3dd5e3078124c4d441fbbf
zhangyongyong <zyy123456>
2018-12-05 12:06:14 +0800

    #

--- jquery.extend.js
+++ jquery.extend.js
@@ -8,12 +8,12 @@ $.extend({
             }
             var data = $.extend({}, ajaxCfg.data || {}, array[ix]);
             var func = ajaxCfg.success || function (data) {
-                };
+            };
             var successFunc = function (data) {
                 func(data);
                 _loopAjax(ix + 1, array, ajaxCfg, finishFunc);
             };
-            var newAjaxParams = $.extend({}, {dataType: 'json', type: 'get'}, ajaxCfg, {
+            var newAjaxParams = $.extend({}, { dataType: 'json', type: 'get' }, ajaxCfg, {
                 data: data,
                 success: successFunc
             });
